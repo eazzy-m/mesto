aef584da4949bb7a20641dd2520186424543dfee
eazzy_m <nikolajkozich@gmail.com>
2021-11-21 16:09:54 +0300

    edit popup__container.css, js file, h3 to h2 in index.html, margin o auto in media-screen in profile__info-subtitle.css and profile__info-title.css

--- scripts/edit-profile.js
+++ scripts/edit-profile.js
@@ -1,7 +1,7 @@
 let container = document.querySelector('.page');
 let editButton = container.querySelector('.edit-button');
 let exitButton = container.querySelector('.exit-button');
-let saveButton = container.querySelector('.form__button');
+let saveButton = container.querySelector('.form');
 let popup = container.querySelector('.popup');
 let name = container.querySelector('.form__item_info_title');
 let job = container.querySelector('.form__item_info_subtitle');
@@ -10,15 +10,16 @@ let profileSubtitle = container.querySelector('.profile__info-subtitle');
 
 function showForm() {
     popup.classList.remove('popup_close');
-    name.placeholder = profileTitle.textContent;
-    job.placeholder = profileSubtitle.textContent;
+    name.value = profileTitle.textContent;
+    job.value = profileSubtitle.textContent;
 }
 
 function closeForm() {
     popup.classList.add('popup_close');
 }
 
-function editProfile() {
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     if (name.value && job.value) {
         profileTitle.textContent = name.value;
         profileSubtitle.textContent = job.value;
@@ -30,4 +31,4 @@ function editProfile() {
 
 editButton.addEventListener('click', showForm);
 exitButton.addEventListener('click', closeForm);
-saveButton.addEventListener('click', editProfile);
+saveButton.addEventListener('submit', formSubmitHandler);
