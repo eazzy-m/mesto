582337ec48e35af851cb6239288ea00f25e77f57
eazzy_m <nikolajkozich@gmail.com>
2021-11-26 15:33:11 +0300

    addcardfunction, likefunction

--- scripts/edit-profile.js
+++ scripts/edit-profile.js
@@ -1,19 +1,27 @@
 let container = document.querySelector('.page');
+
 let editButton = container.querySelector('.edit-button');
-let exitButton = container.querySelector('.exit-button');
+let addButton = container.querySelector('.add-button');
+let exitProfileButton = container.querySelector('.exit-button_popup_profile');
+let exitElementsButton = container.querySelector('.exit-button_popup_elements');
+let exitZoomImageButton = container.querySelector('.exit-button_zoom-image');
 let saveButton = container.querySelector('.edit-profile-form');
-let popup = container.querySelector('.popup');
+
+
+let popupEditProfile = container.querySelector('.popup_profile');
+let popupAddCards = container.querySelector('.popup_elements');
+let popupZoomImage = container.querySelector('.popup_zoom-image');
+
 let name = container.querySelector('.form__item_info_title');
 let job = container.querySelector('.form__item_info_subtitle');
-const cardTemplate = document.querySelector('#element-template').content;
 let profileTitle = container.querySelector('.profile__info-title');
 let profileSubtitle = container.querySelector('.profile__info-subtitle');
-let addButton = container.querySelector('.add-button');
-let editProfileForm = container.querySelector('.form_close');
+let editProfileForm = container.querySelector('.form');
 let addCardForm = container.querySelector('.add-card-form');
-let elements = container.querySelector('.elements'); // analog songsContainer
 
 
+const cardTemplate = document.querySelector('#element-template').content;
+let elements = container.querySelector('.elements');
 const initialCards = [
     {
         name: 'Архыз',
@@ -41,27 +49,19 @@ const initialCards = [
     }
 ];
 
-function releaseDefaultCards(element) {
-    const card = cardTemplate.querySelector('.element').cloneNode(true);
-    card.querySelector('.element__text').textContent = element.name;
-    card.querySelector('.element__mask-group').src = element.link;
-
-    elements.append(card);
-}
-
-
 function addCards(evt) {
     evt.preventDefault();
     const card = cardTemplate.querySelector('.element').cloneNode(true);
 
     card.querySelector('.element__text').textContent = container.querySelector('.form__item_place-name').value;
     card.querySelector('.element__mask-group').src = container.querySelector('.form__item_place-image-source').value;
+    card.querySelector('.element__mask-group').alt = container.querySelector('.form__item_place-name').value;
     createCard(card);
     elements.prepend(card);
     container.querySelector('.form__item_place-name').value = '';
     container.querySelector('.form__item_place-image-source').value = '';
 
-    closeForm();
+    closeAddCardsForm();
 }
 
 function addDefaultCards(element) {
@@ -84,41 +84,69 @@ function createCard(obj) {
         evt.target.classList.toggle('like-button_active');
     })
     // delete
-    const deleteCard = obj.querySelector('.remove-element-button');
+    const deleteCard = obj.querySelector('.delete-element-button');
     deleteCard.addEventListener('click' ,function (evt) {
         const element = deleteCard.closest('.element');
         element.remove();
     })
     // zoom
-    // const zoom = obj.querySelector('.element__mask-group');
+    const img = obj.querySelector('.element__mask-group');
+    img.addEventListener('click', function () {
+        PopupZoomImage();
+    container.querySelector('.popup__figure-img').src = img.src;
+    container.querySelector('.popup__figcaption').textContent= img.alt;
+    })
 
 }
 
-addCardForm.addEventListener('submit', addCards);
+function PopupEditProfile() {
+    popupEditProfile.classList.add('popup_open');
+}
 
-function showPopup() {
-    popup.classList.remove('popup_close');
+function PopupAddCards() {
+    popupAddCards.classList.add('popup_open');
+}
+
+function PopupZoomImage() {
+    popupZoomImage.classList.add('popup_open');
 }
 
 function showEditProfileForm() {
-    showPopup();
-    editProfileForm.classList.remove('form_close');
+    PopupEditProfile();
+    editProfileForm.classList.add('form_open');
     name.value = profileTitle.textContent;
     job.value = profileSubtitle.textContent;
 }
 
 function showAddCardForm() {
-    showPopup();
-    addCardForm.classList.remove('add-form_close');
+    PopupAddCards();
+    addCardForm.classList.add('add-form_open');
 }
 
-function closeForm() {
-    popup.classList.add('popup_close');
-    if (editProfileForm.classList.contains('form_close')) {
-        addCardForm.classList.add('add-form_close');
-    } else {
-        editProfileForm.classList.add('form_close');
-    }
+// function closeForm() {
+//     if ( popupEditProfile.classList.contains('popup_open')) {
+//         popupEditProfile.classList.remove('popup_open');
+//         editProfileForm.classList.add('form_close');
+//     } else if (popupAddCards.classList.contains('popup_open')) {
+//         popupAddCards.classList.remove('popup_open');
+//         addCardForm.classList.add('add-form_close');
+//     } else if (popupZoomImage.classList.contains('popup_open')) {
+//         popupZoomImage.classList.remove('popup_open');
+//     }
+// }
+
+function closeProfileForm() {
+    popupEditProfile.classList.remove('popup_open');
+    editProfileForm.classList.remove('form_open');
+}
+
+function closeAddCardsForm() {
+    popupAddCards.classList.remove('popup_open');
+    addCardForm.classList.remove('add-form_open');
+}
+
+function closeZoomImage() {
+    popupZoomImage.classList.remove('popup_open');
 }
 
 function formSubmitHandler(evt) {
@@ -126,17 +154,19 @@ function formSubmitHandler(evt) {
     if (name.value && job.value) {
         profileTitle.textContent = name.value;
         profileSubtitle.textContent = job.value;
-        closeForm();
+        PopupEditProfile();
         name.value = '';
         job.value = '';
     }
+    closeProfileForm();
 }
 
-
-
+exitZoomImageButton.addEventListener('click', closeZoomImage)
+addCardForm.addEventListener('submit', addCards);
 addButton.addEventListener('click', showAddCardForm);
 editButton.addEventListener('click', showEditProfileForm);
-exitButton.addEventListener('click', closeForm);
+exitProfileButton.addEventListener('click', closeProfileForm);
+exitElementsButton.addEventListener('click', closeAddCardsForm);
 saveButton.addEventListener('submit', formSubmitHandler);
 
 
