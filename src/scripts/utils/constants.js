0496a1e5b259cf787bdfd14a806cb2f71e7fb0e6
nikolaykozich <nikolay.kozich@adx-team.com>
2022-02-04 14:27:38 +0300

    add renderItems method to Section; api.getDefaultData return initial cards in catch case

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -9,3 +9,96 @@ export const job = document.querySelector('#subtitle-input');
 export const avatarButton = document.querySelector('.profile__avatar');
 export const editButton = document.querySelector('.edit-button');
 export const addButton = document.querySelector('.add-button');
+
+export const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+        createdAt: "2022-02-04T09:26:27.195Z",
+        owner:
+            {
+                about: "unknown",
+                avatar: "unknown",
+                cohort: "unknown",
+                name: "unknown",
+                _id: "0",
+            },
+        likes: [],
+        id : 0
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+        createdAt: "2022-02-04T09:26:27.195Z",
+        owner:
+            {
+                about: "unknown",
+                avatar: "unknown",
+                cohort: "unknown",
+                name: "unknown",
+                _id: "0",
+            },
+    likes: [],
+        id : 0
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+        createdAt: "2022-02-04T09:26:27.195Z",
+        owner:
+            {
+                about: "unknown",
+                avatar: "unknown",
+                cohort: "unknown",
+                name: "unknown",
+                _id: "0",
+            },
+        likes: [],
+        id : 0
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+        createdAt: "2022-02-04T09:26:27.195Z",
+        owner:
+            {
+                about: "unknown",
+                avatar: "unknown",
+                cohort: "unknown",
+                name: "unknown",
+                _id: "0",
+            },
+        likes: [],
+        id : 0
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+        createdAt: "2022-02-04T09:26:27.195Z",
+        owner:
+            {
+                about: "unknown",
+                avatar: "unknown",
+                cohort: "unknown",
+                name: "unknown",
+                _id: "0",
+            },
+        likes: [],
+        id : 0
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+        createdAt: "2022-02-04T09:26:27.195Z",
+        owner:
+            {
+                about: "unknown",
+                avatar: "unknown",
+                cohort: "unknown",
+                name: "unknown",
+                _id: "0",
+            },
+        likes: [],
+        id : 0
+    }
+];
